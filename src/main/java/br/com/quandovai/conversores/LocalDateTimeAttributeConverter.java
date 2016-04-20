4313241b834a52edf9c5afe18a72666889fffd6e
leonardo <leo.zapparoli@gmail.com>
2016-04-20 18:00:58 -0300

    conversor de datas

---
+++ src/main/java/br/com/quandovai/conversores/LocalDateTimeAttributeConverter.java
@@ -0,0 +1,30 @@
+package br.com.quandovai.conversores;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+import javax.persistence.AttributeConverter;
+
+public class LocalDateTimeAttributeConverter implements AttributeConverter<LocalDateTime, Date> {
+
+	@Override
+	public Date convertToDatabaseColumn(LocalDateTime attribute) {
+		if (attribute == null)
+			return null;
+		Instant instant = attribute.toInstant(ZoneOffset.ofHours(-3));
+		Date fromInstant = Date.from(instant);
+		return fromInstant;
+	}
+
+	@Override
+	public LocalDateTime convertToEntityAttribute(Date dbData) {
+		if (dbData == null)
+			return null;
+		Instant instant = Instant.ofEpochMilli(dbData.getTime());
+		return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+	}
+
+}
